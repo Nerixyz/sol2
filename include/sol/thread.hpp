43e24c2cf0615624d75c9d1f76ab02111ed52cae
Nerixyz <nerixdev@outlook.de>
2026-06-12 23:00:38 +0200

    fix: thread

--- include/sol/thread.hpp
+++ include/sol/thread.hpp
@@ -24,6 +24,7 @@
 #ifndef SOL_THREAD_HPP
 #define SOL_THREAD_HPP
 
+#include "lua.h"
 #include <sol/reference.hpp>
 #include <sol/object.hpp>
 #include <sol/stack.hpp>
@@ -158,7 +159,11 @@ namespace sol {
 			auto lstat = static_cast<thread_status>(lua_status(lthread));
 			if (lstat == thread_status::ok) {
 				lua_Debug ar;
+#if SOL_IS_ON(SOL_USE_LUAU)
+				if (lua_getinfo(lthread, 0, "", &ar) > 0)
+#else
 				if (lua_getstack(lthread, 0, &ar) > 0)
+#endif
 					return thread_status::ok;
 				else if (lua_gettop(lthread) == 0)
 					return thread_status::dead;
