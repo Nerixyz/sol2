50eb63124db06833e175f67c3c9eee93c4a315b0
Nerixyz <nerixdev@outlook.de>
2026-06-20 14:32:23 +0200

    fix: array access

--- include/sol/stack_field.hpp
+++ include/sol/stack_field.hpp
@@ -28,6 +28,7 @@
 #include <sol/stack_push.hpp>
 #include <sol/stack_get.hpp>
 #include <sol/stack_check_get.hpp>
+#include <sol/traits.hpp>
 
 namespace sol { namespace stack {
 
@@ -118,7 +119,7 @@ namespace sol { namespace stack {
 				}
 				else if constexpr (std::is_same_v<T, meta_function>) {
 					const auto& real_key = to_string(key);
-					lua_getfield(L, tableindex, &real_key[0]);
+					lua_getfield(L, tableindex, real_key.c_str());
 				}
 #if SOL_LUA_VERSION_I_ >= 503
 				else if constexpr (std::is_integral_v<T> && !std::is_same_v<bool, T>) {
@@ -215,12 +216,22 @@ namespace sol { namespace stack {
 				if constexpr (meta::is_c_str_or_string_v<T>) {
 					if constexpr (global) {
 						push(L, std::forward<Value>(value));
-						lua_setglobal(L, &key[0]);
+						if constexpr (meta::is_c_str_v<T>) {
+							lua_setglobal(L, key);
+						}
+						else {
+							lua_setglobal(L, std::data(key));
+						}
 						(void)tableindex;
 					}
 					else {
 						push(L, std::forward<Value>(value));
-						lua_setfield(L, tableindex, &key[0]);
+						if constexpr (meta::is_c_str_v<T>) {
+							lua_setfield(L, tableindex, key);
+						}
+						else {
+							lua_setfield(L, tableindex, std::data(key));
+						}
 					}
 				}
 #if SOL_LUA_VERSION_I_ >= 503
