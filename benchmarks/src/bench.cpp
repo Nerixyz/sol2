839ef502057fdbbe121cca1223a614c2e99792a9
Nerixyz <nerixdev@outlook.de>
2026-07-06 22:00:10 +0200

    fix: luau, -werror, and ctest

--- benchmarks/src/bench.cpp
+++ benchmarks/src/bench.cpp
@@ -23,7 +23,7 @@ namespace sol::bench {
 	}
 
 	void luaDoStringOrThrow(lua_State* L, std::string_view code, std::string_view where) {
-		const int status = luaL_dostring(L, std::string(code).c_str());
+		const int status = luaL_loadstring(L, std::string(code).c_str()) || lua_pcall(L, 0, LUA_MULTRET, 0);
 		luaCheckOrThrow(L, status, where);
 	}
 
